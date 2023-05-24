52e4233f703e9c261e76ed853adebf709b28ba5b
Matt Hammond <mjhammond94@gmail.com>
2023-05-24 17:21:55 +0100

    Add thermostat files - Add first test - Test pass

---
+++ thermostat.test.js
@@ -0,0 +1,13 @@
+const Thermostat = require('./thermostat');
+
+describe(Thermostat, () => {
+  it('Should return 20 when getTemperature is called', () => {
+    const therm1 = new Thermostat();
+    expect(therm1.getTemperature()).toBe(20);
+  });
+
+  it('Should increase the temperature by 1 when up method is called', () => {
+    const therm1 = new Thermostat();
+    expect(therm1.up()).toBe(21);
+  });
+});
