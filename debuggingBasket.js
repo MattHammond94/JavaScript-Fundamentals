b263671a21c242eaf55d8a1f22cb4150c727c493
Matt Hammond <mjhammond94@gmail.com>
2023-05-24 17:07:59 +0100

    Resolve bug

--- debuggingBasket.js
+++ debuggingBasket.js
@@ -1,10 +1,17 @@
+const Candy = require('./candy')
+
 class ShoppingBasket {
   constructor() {
     this.discount = 0;
+    this.candies = [];
+  }
+
+  addCandy(candy) {
+    this.candies.push(candy); 
   }
 
   applyDiscount(discount) {
-    discount = this.discount;
+    this.discount += discount;
   }
 
   getTotalPrice() {
@@ -16,3 +23,10 @@ class ShoppingBasket {
     return totalPrice - this.discount;
   }
 }
+
+const basket1 = new ShoppingBasket;
+basket1.applyDiscount(10);
+basket1.addCandy(new Candy('Jelly-Beans', 14.99));
+basket1.addCandy(new Candy('BonBons', 9.99));
+basket1.addCandy(new Candy('Maoam', 5.99));
+console.log(basket1.getTotalPrice());
