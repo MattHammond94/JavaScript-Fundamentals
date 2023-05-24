171ff49aaca5221b5f482082f1f9306654e2cba4
Matt Hammond <mjhammond94@gmail.com>
2023-05-24 16:05:18 +0100

    Add test, Add method for candy.js, Test pass

--- candy.js
+++ candy.js
@@ -2,10 +2,15 @@ class Candy {
   
   constructor(name, price) {
     this.name = name;
+    this.price = price;
   }
 
   getName() {
-    return this.name 
+    return this.name;
+  }
+
+  getPrice() {
+    return this.price;
   }
 
 }
--- candy.test.js
+++ candy.test.js
@@ -2,9 +2,14 @@ const Candy = require('./candy');
 
 describe(Candy, () => {
   it('Should return the correct name when getName is called', () => {
-    // const randomCandy = { getName: () => 'Marshmellows' };
-    const candy1 = new Candy('KitKat', 2.99);
-    expect(candy1.getName()).toBe('KitKat');
-    // expect(randomCandy.getName()).toBe('Marshmellows');
+    const randomCandy = { getName: () => 'Marshmellows' };
+    // const candy1 = new Candy('KitKat', 2.99);
+    // expect(candy1.getName()).toBe('KitKat');
+    expect(randomCandy.getName()).toBe('Marshmellows');
+  });
+
+  it('Should return the correct price when getPrice is called', () => {
+    const candy2 = new Candy('Wispa', 4.99);
+    expect(candy2.getPrice()).toBe(4.99);
   });
 });
