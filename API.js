6d82f5d570799bc1e529ffb4ca52100d0bad8a9a
Matt Hammond <mjhammond94@gmail.com>
2023-05-25 15:49:14 +0100

    update pokemon complete

--- API.js
+++ API.js
@@ -1,9 +1,27 @@
 const fetchPokemon = (pokemonname) => {
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonname}`)
   .then((response) => response.json())
-  .then((pokemon) => console.log(pokemon));
+  .then((pokemon) => 
+  
+  // console.log(pokemon.types));
+  
+  console.log({
+    name: pokemon.name,
+    id: pokemon.id,
+    height: pokemon.height,
+    weight: pokemon.weight,
+    types: pokemon.types.map((slot) => {
+      return slot.type.name;
+    })
+    
+    // .forEach((type) => {
+    //   type.type.name
+    // })
+  }));
 }
 
-// console.log(fetchPokemon('oddish'));
+console.log(fetchPokemon('oddish'));
 
 module.exports = fetchPokemon;
+
+// pokemon.types[0].type.name
